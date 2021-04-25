841e43cb9747594d5553aa68b50494e8d82f8179
SebastianRochert <sebastian.rochert@t-online.de>
2021-04-25 12:17:22 +0200

    UserDTO Interface und UserDTOimpl hinzugefügt und vorläufig "befüllt".

--- src/dtos/UserDTO.java
+++ src/dtos/UserDTO.java
@@ -1,4 +1,11 @@
 package dtos;
 
+import java.util.List;
+
 public interface UserDTO {
+    public int getId();
+    public String getFirstName();
+    public String getLastName();
+    public String getEMail();
+    //public List<RolleDTO> getRoles();
 }
--- src/dtos/impl/UserDTOimpl.java
+++ src/dtos/impl/UserDTOimpl.java
@@ -1,4 +1,44 @@
 package dtos.impl;
 
+import java.util.List;
+
 public class UserDTOimpl {
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String eMail;
+    //private List<RolleDTO> roles;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+        return this.firstname;
+    }
+
+    public String getLastName() {
+        return this.lastname;
+    }
+
+    public String geteMail() {
+        return this.eMail;
+    }
+    /*
+    @Override
+    public List<RolleDTO> getRoles() {
+        return this.roles;
+    }
+     */
+
+    @Override
+    public String toString() {
+        return "UserDTOImpl{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", E-Mail='" + eMail +
+                //", roles=" + roles +
+                '}';
+    }
 }
