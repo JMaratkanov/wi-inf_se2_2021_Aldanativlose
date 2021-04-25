2d7c0e6851516746e48544ccc94c5b87c0aeb6e9
SebastianRochert <sebastian.rochert@t-online.de>
2021-04-25 14:25:18 +0200

    Factory Method Pattern umgesetzt.

--- src/dtos/impl/UserDTOimpl.java
+++ src/dtos/impl/UserDTOimpl.java
@@ -7,35 +7,6 @@ public class UserDTOimpl implements UserDTO {
     private String firstname;
     private String lastname;
 
-    public UserDTOimpl createUser(int id, String firstname, String lastname) {
-        UserDTOimpl x = new UserDTOimpl();
-        x.setId(id);
-        x.setFirstname(firstname);
-        x.setLastname(lastname);
-        return x;
-    }
-
-    public UserDTOimpl createUserIF(int id, String firstname) {
-        UserDTOimpl x = new UserDTOimpl();
-        x.setId(id);
-        x.setFirstname(firstname);
-        return x;
-    }
-
-    public UserDTOimpl createUserIL(int id, String lastname) {
-        UserDTOimpl x = new UserDTOimpl();
-        x.setId(id);
-        x.setLastname(lastname);
-        return x;
-    }
-
-    public UserDTOimpl createUserFL(String firstname, String lastname) {
-        UserDTOimpl x = new UserDTOimpl();
-        x.setFirstname(firstname);
-        x.setLastname(lastname);
-        return x;
-    }
-
     public void setId(int id) {
         this.id = id;
     }
--- src/test/RegistrationTest.java
+++ src/test/RegistrationTest.java
@@ -1,8 +1,10 @@
 package test;
 
 import control.RegistrationControl;
+import control.factory.Factory;
 import dtos.UserDTO;
 import dtos.impl.UserDTOimpl;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,20 +12,21 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RegistrationTest {
-    private UserDTO a = null;
-    private UserDTO b = null;
-    private UserDTO c = null;
-    private UserDTO d = null;
-    private UserDTO e = null;
-
+    private UserDTO user1 = null;
     RegistrationControl rc = new RegistrationControl();
 
     @BeforeEach
     void setup() {
+        user1 = Factory.createUser(1, "Max", "Mustermann");
+    }
+
+    @AfterEach
+    void teardown() {
+        user1 = null;
     }
 
     @Test
     void testRegistrationControl() {
-        assertEquals("Registrierung erfolgreich", rc.registerUser(a), "Fehler bei der Registrierung");
+        assertEquals("Registrierung erfolgreich", rc.registerUser(user1), "Fehler bei der Registrierung");
     }
 }
