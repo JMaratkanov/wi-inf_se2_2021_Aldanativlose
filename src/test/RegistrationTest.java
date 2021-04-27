21346107716c05026635509f3d5f1a5afb00cea2
SebastianRochert <sebastian.rochert@t-online.de>
2021-04-27 17:33:12 +0200

    Test Klassen zusammengefasst.

--- src/test/RegistrationTest.java
+++ src/test/RegistrationTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import control.RegistrationControl;
+import control.builder.UserBuilder;
 import control.factory.Factory;
 import dtos.UserDTO;
 import org.junit.jupiter.api.AfterEach;
@@ -18,6 +19,7 @@ public class RegistrationTest {
     private UserDTO user5 = null;
     private UserDTO user6 = null;
     RegistrationControl rc = null;
+    UserBuilder ub = null;
 
     @BeforeEach
     void setup() {
@@ -28,6 +30,7 @@ public class RegistrationTest {
         user5 = Factory.createUser(4, "A", "Meyer");
         user6 = Factory.createUser(5, "Alexander", "M");
         rc = new RegistrationControl();
+        ub = new UserBuilder();
     }
 
     @AfterEach
@@ -36,11 +39,36 @@ public class RegistrationTest {
         user2 = null;
         user3 = null;
         user4 = null;
+        user5 = null;
+        user6 = null;
         rc = null;
     }
 
     @Test
     void testRegistrationControl() {
+        assertEquals("Registrierung erfolgreich!", rc.registerUser(user1), "Fehler im RegistrationControl");
+        assertEquals("Kein Nachname angegeben!", rc.registerUser(user2), "Fehler im RegistrationControl");
+        assertEquals("Kein Vorname angegeben!", rc.registerUser(user3), "Fehler im RegistrationControl");
+        assertEquals("Keine ID angegeben!", rc.registerUser(user4), "Fehler im RegistrationControl");
+        assertEquals("Vorname ist zu kurz!", rc.registerUser(user5), "Fehler im RegistrationControl");
+        assertEquals("Nachname ist zu kurz!", rc.registerUser(user6), "Fehler im RegistrationControl");
+    }
+
+    @Test
+    void testRegistrationControlBuilder() {
+        /*
+        How to use this builder: Create instance -> Use instance to build an empty user (has neither attributes set ...
+        like when you construct a UserDTO with "new UserDTOimpl()"
+        Default creates User with ID: 1337 FN: Obi-Wan LN: Kenobi
+        */
+
+        user1 = ub.buildEmptyUser().withDefaultID().withDefaultFirstName().withDefaultLastName().done();
+        user2 = ub.buildEmptyUser().withThisID(2).withThisFirstName("Toni").done();
+        user3 = ub.buildEmptyUser().withThisID(3).withThisLastName("Müller").done();
+        user4 = ub.buildEmptyUser().withThisFirstName("Eren").withThisLastName("Jäger").done();
+        user5 = ub.buildEmptyUser().withThisID(4).withThisFirstName("A").withThisLastName("Meyer").done();
+        user6 = ub.buildEmptyUser().withThisID(5).withThisFirstName("Alexander").withThisLastName("M").done();
+
         assertEquals("Registrierung erfolgreich!", rc.registerUser(user1), "Fehler im RegistrationControl");
         assertEquals("Kein Nachname angegeben!", rc.registerUser(user2), "Fehler im RegistrationControl");
         assertEquals("Kein Vorname angegeben!", rc.registerUser(user3), "Fehler im RegistrationControl");
