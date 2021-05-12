11a973eda621551f9defcb3846bf79a8784be5c3
SebastianRochert <sebastian.rochert@t-online.de>
2021-05-12 10:10:05 +0200

    Main Class (CollatHBRSApplication) erstellt.

---
+++ src/main/java/Views/CollatHBRSApplication.java
@@ -0,0 +1,12 @@
+package Views;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollatHBRSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollatHBRSApplication.class, args);
+    }
+}
