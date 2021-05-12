1315e85491a542be156e19ec6da79f20e7d68654
SebastianRochert <sebastian.rochert@t-online.de>
2021-05-12 10:25:38 +0200

    Erweiterung um weitere Felder.

--- src/main/java/Views/RegistrationView.java
+++ src/main/java/Views/RegistrationView.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -17,32 +18,42 @@ public class RegistrationView extends Composite {
 
     @Override
     protected Component initContent() {
-        TextField username = new TextField("Username");
-        PasswordField password1 = new PasswordField("Password");
-        PasswordField password2 = new PasswordField("Confirm password");
+        TextField firstName = new TextField("Vorname");
+        TextField lastName = new TextField("Nachname");
+        EmailField email = new EmailField("E-Mail");
+        PasswordField password1 = new PasswordField("Passwort");
+        PasswordField password2 = new PasswordField("Passwort bestätigen");
         return new VerticalLayout(
                 new H2("Register"),
-                username,
+                firstName,
+                lastName,
+                email,
                 password1,
                 password2,
                 new Button("Send", event -> register(
-                        username.getValue(),
+                        firstName.getValue(),
+                        lastName.getValue(),
+                        email.getValue(),
                         password1.getValue(),
                         password2.getValue()
                 ))
         );
     }
 
-    private void register(String username, String password1, String password2) {
-        if (username.trim().isEmpty()) {
-            Notification.show("Enter a username");
+    private void register(String firstName, String lastName, String email, String password1, String password2) {
+        if (firstName.trim().isEmpty()) {
+            Notification.show("Geben Sie Ihren Vornamen an");
+        } else if (lastName.trim().isEmpty()) {
+            Notification.show("Geben Sie Ihren Nachnamen an");
+        } else if (email.isEmpty()) {
+            Notification.show("Geben Sie Ihre E-Mail Adresse an");
         } else if (password1.isEmpty()) {
-            Notification.show("Enter a password");
+            Notification.show("Geben Sie ein Passwort an");
         } else if (!password1.equals(password2)) {
-            Notification.show("Passwords don't match");
+            Notification.show("Passwörter stimmen nicht überein");
         } else {
             //authService.register(username, password1);
-            Notification.show("Check your email.");
+            Notification.show("E-Mail Bestätigung versendet!");
         }
     }
 }
