3f9faaea0b78993152d8b2f9e228346d22526a09
SebastianRochert <sebastian.rochert@t-online.de>
2021-05-27 18:40:32 +0200

    CollatHBRSApplication nun von SpringBootServletInitializer abgeleitet.

--- src/main/java/ui/CollatHBRSApplication.java
+++ src/main/java/ui/CollatHBRSApplication.java
@@ -2,10 +2,11 @@ package ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class CollatHBRSApplication {
+public class CollatHBRSApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CollatHBRSApplication.class, args);
