b35903599564fdf49666a6668b7895f078893611
SebastianRochert <sebastian.rochert@t-online.de>
2021-05-30 18:58:26 +0200

    getEmail ergänzt

--- src/main/java/dtos/UserDTO.java
+++ src/main/java/dtos/UserDTO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public interface UserDTO {
     public int getId();
+    public String getEmail();
     public String getFirstName();
     public String getLastName();
     //public List<RolleDTO> getRoles();
