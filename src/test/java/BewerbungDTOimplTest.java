2289c07c25e02462fd5f8ad5d12d45823bfe2006
Kai Friese <okorian@web.de>
2021-06-29 13:12:42 +0200

    BewerbungDTOimplTest

---
+++ src/test/java/BewerbungDTOimplTest.java
@@ -0,0 +1,73 @@
+import dtos.impl.BewerbungDTOimpl;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BewerbungDTOimplTest {
+    public static BewerbungDTOimpl bewerbung;
+
+    @BeforeClass
+    public static void setup(){
+        bewerbung = new BewerbungDTOimpl();
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        bewerbung = null;
+    }
+
+    @Test
+    public void idTest(){
+        assertEquals(0, bewerbung.getID());
+        bewerbung.setID(1);
+        assertEquals(1, bewerbung.getID());
+    }
+
+    @Test
+    public void nameTest(){
+        assertEquals(null, bewerbung.getName());
+        bewerbung.setName("test");
+        assertEquals("test", bewerbung.getName());
+    }
+
+    @Test
+    public void unternehmenTest(){
+        assertEquals(null, bewerbung.getUnternehmen());
+        bewerbung.setUnternehmen("test");
+        assertEquals("test", bewerbung.getUnternehmen());
+    }
+
+    @Test
+    public void statusTest(){
+        assertEquals(null, bewerbung.getStatus());
+        bewerbung.setStatus(0);
+        assertEquals("abgelehnt", bewerbung.getStatus());
+        bewerbung.setStatus(1);
+        assertEquals("Stelle noch verfügbar", bewerbung.getStatus());
+        bewerbung.setStatus(2);
+        assertEquals("Ausschreibung beendet", bewerbung.getStatus());
+    }
+
+    @Test
+    public void mehrTest(){
+        assertEquals(null, bewerbung.getMehr());
+        bewerbung.setMehr("test");
+        assertEquals("test", bewerbung.getMehr());
+    }
+
+    @Test
+    public void inseratIDTest(){
+        assertEquals(0, bewerbung.getInseratID());
+        bewerbung.setInseratID(1);
+        assertEquals(1, bewerbung.getInseratID());
+    }
+
+    @Test
+    public void unternehmenIDTest(){
+        assertEquals(0, bewerbung.getUnternehmenID());
+        bewerbung.setUnternehmenID(1);
+        assertEquals(1, bewerbung.getUnternehmenID());
+    }
+}
