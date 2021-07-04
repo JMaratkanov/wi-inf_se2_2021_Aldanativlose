297b464c430246077194d89df06d2dbc7e6c6bdb
SebastianRochert <sebastian.rochert@t-online.de>
2021-07-04 20:36:57 +0200

    Spalten im zweiten Grid der StellenanzeigenView für Unternehmer ausgeblendet

--- src/main/java/ui/appViews/AdView.java
+++ src/main/java/ui/appViews/AdView.java
@@ -216,6 +216,11 @@ public class AdView extends Div {
         grid.addColumn(StellenanzeigeDTOimpl::getInseratTyp).setHeader("Inserat Typ").setSortable(true).setFlexGrow(0).setWidth("200px").setKey("typeColum");
         grid.addColumn(StellenanzeigeDTOimpl::getStatus).setHeader("Status").setSortable(true).setFlexGrow(0).setWidth("250px").setKey("statusColum");
 
+        if(ownAds) {
+            //grid.getColumnByKey("startColum").setVisible(false);
+            grid.getColumnByKey("hoursColum").setVisible(false);
+            grid.getColumnByKey("placeColum").setVisible(false);
+        }
 
         grid.addItemClickListener(event -> {
                 Dialog d = new Dialog();
